ea8b6e3e9f423baf8f07d64ac0dd870957c087cd
aargento <aargento@vulog.com>
2020-06-02 16:41:28 +0200

    creating Lavanderia stuff

--- demo/src/main/java/com/argento/lavan/controllers/LavanderiaController.java
+++ demo/src/main/java/com/argento/lavan/controllers/LavanderiaController.java
@@ -5,27 +5,34 @@ import java.util.List;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.argento.lavan.dto.LavanderiaDto;
 import com.argento.lavan.entities.Lavanderia;
 
 @RestController(value = "/lavanderie")
 public class LavanderiaController {
 	
-	@GetMapping
+	@GetMapping(produces = "application/json")
 	public ResponseEntity<List<Lavanderia>> getLavanderie(){
 		return null;
 	}
 	
-	@GetMapping(value="{id}")
-	public ResponseEntity<Lavanderia> getLavanderia(){
+	@GetMapping(value="{id}", produces = "application/json")
+	public ResponseEntity<Lavanderia> getLavanderia(
+			@PathVariable String id){
+		
 		return null;
 	}
 
-	@PostMapping(consumes="")
-	public ResponseEntity<Void> createLavanderia(){
+	@PostMapping(consumes = "application/json", produces = "application/json")
+	public ResponseEntity<Void> createLavanderia(
+			@PathVariable String fleetId,
+            @RequestBody LavanderiaDto lavanderiaDto){
 		return null;
 	}
 	
